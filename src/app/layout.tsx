bba3504106d97497297859398481bb760f5ac783
tqlong1609 <tqlong1609@gmail.com>
2024-08-08 23:04:03 +0700

    Revert "style: add styles common"
    This reverts commit 9698bd4d6f00a2fa30664de9a1b55b5fef85003d.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import BootstrapClient from '@/components/BootstrapClient'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import '../scss/style.scss'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,10 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <BootstrapClient />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <main className="container">
-        <h1 className="text-center">NextJS Application</h1>
-        <button className="btn-theme m-2">Button</button>
-        <button className="btn btn-secondary m-2">Button</button>
-        <button className="btn btn-danger m-2">Button</button>
-      </main>
-    </div>
-  )
+  return <div>home</div>
 }
