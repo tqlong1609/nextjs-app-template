cffca691fa0a39aefc6a37affe27e22a43fdc796
trung.tra <trung.tra@ascendcorp.com>
2024-08-19 13:59:34 +0700

    feat: setup react query, example for fetch data

---
+++ src/app/initial-data/page.tsx
@@ -0,0 +1,9 @@
+import { getUsers } from '@/utils/api-requests'
+
+import ListUsers from './list-users'
+
+export default async function InitialData() {
+  const users = await getUsers()
+
+  return <ListUsers users={users} />
+}
