06f2446e40b1082bb8e0c4a4f369d58d4ba890cb
trung.tra <trung.tra@ascendcorp.com>
2024-08-19 13:59:34 +0700

    feat: setup react query, example for fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div>
+      <h1>Hello, Next.js 14 App Directory!</h1>
+      <p>
+        <Link prefetch href="/initial-data">
+          Prefetching Using initial data --- Good for SEO
+        </Link>
+      </p>
+      <p>
+        <Link prefetch href="/hydration">
+          Prefetching Using Hydration --- Good for SEO
+        </Link>
+      </p>
+    </div>
+  )
 }
